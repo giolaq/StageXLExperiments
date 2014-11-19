c375852c9bfb1a1cf6464dd316894bd96627b0ba
joaobiriba <glaquidara@gmail.com>
2014-11-19 17:43:25 +0100

    Slides work in progress, animation and text

--- web/dartsideofwebdevelopment.dart
+++ web/dartsideofwebdevelopment.dart
@@ -6,22 +6,32 @@ Random random = new Random();
 Stage stage;
 RenderLoop renderLoop;
 
-class Painting extends DisplayObjectContainer {
+class Slide extends DisplayObjectContainer {
   final List<int> colors = [Color.Red];
+  String mText;
+  int mIndex = 0;
 
-  Painting() {
-    var background = new BitmapData(100, 100, false, Color.BlanchedAlmond);
-    var backgroundBitmap = new Bitmap(background);
-    addChild(backgroundBitmap);
+  Slide(this.mText, this.mIndex) {
 
-    for (var i = 0; i < colors.length; i++) {
-      var box = new BitmapData(100, 100, false, colors[i]);
-      var boxBitmap = new Bitmap(box);
-      boxBitmap.x = 0;
-      boxBitmap.y = 0;
-      addChild(boxBitmap);
-    }
+
+    var box = new BitmapData(100, 100, false, Color.Red);
+    var boxBitmap = new Bitmap(box);
+    boxBitmap.x = 0 + 100 * mIndex;
+    boxBitmap.y = 0;
+    addChild(boxBitmap);
+
+
+    var textField1 = new TextField();
+    var textFormat1 = new TextFormat('Helvetica,Arial', 14, Color.Green, bold: true, italic: true);
+    textField1.defaultTextFormat = textFormat1;
+    textField1.text = mText;
+    textField1.x = 10 + 100 * mIndex;
+    textField1.y = 10;
+    textField1.width = 920;
+    textField1.height = 20;
+    addChild(textField1);
   }
+
 }
 
 void main() {
@@ -31,32 +41,44 @@ void main() {
   renderLoop.addStage(stage);
   var juggler = renderLoop.juggler;
 
-  var background = new BitmapData(100, 100, false, Color.BlanchedAlmond);
-  var backgroundBitmap = new Bitmap(background);
-  var box = new BitmapData(100, 100, false, Color.Red);
-  var boxBitmap = new Bitmap(box);
-  boxBitmap.x = 0;
-  boxBitmap.y = 0;
-  stage.addChild(boxBitmap);
+  Slide slide1 = new Slide("Prima\nslide", 1);
+  Slide slide2 = new Slide("Seconda\nslide", 2);
 
+  stage.addChild(slide1);
+  stage.addChild(slide2);
 
   stage.focus = stage;
 
 
   stage.onKeyDown.listen((ev) {
-    var tween = new Tween(boxBitmap, 1.0, TransitionFunction.linear);
+    var tween = new Tween(slide1, 0.4, TransitionFunction.linear);
+    var tween2 = new Tween(slide2, 0.4, TransitionFunction.linear);
 
     switch (ev.keyCode) {
       case 38:
         tween.animate.x.to(0);
         tween.animate.y.to(0);
+        tween.animate.alpha.to(1);
+
+        tween2.animate.x.to(350);
+        tween2.animate.y.to(0);
+        tween2.animate.alpha.to(0.5);
+
+
         break;
       case 40:
-        tween.animate.x.to(100);
+        tween.animate.x.to(-100);
         tween.animate.y.to(0);
+        tween.animate.alpha.to(0.5);
+
+        tween2.animate.x.to(0);
+        tween2.animate.y.to(0);
+        tween2.animate.alpha.to(1);
+
         break;
     }
     juggler.add(tween);
+    juggler.add(tween2);
 
 
 
