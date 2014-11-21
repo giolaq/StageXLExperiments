197a9b98d31cebe71840cfec8d3fb98e4264a374
joaobiriba <glaquidara@gmail.com>
2014-11-21 17:39:17 +0100

    forward and back ok

--- web/dartsideofwebdevelopment.dart
+++ web/dartsideofwebdevelopment.dart
@@ -15,49 +15,54 @@ class Slide extends DisplayObjectContainer {
   int mIndex = 0;
   Tween mTween;
   Bitmap boxBitmap;
+  TextField textField1;
 
   Slide(this.mText, this.mIndex) {
 
 
-    var box = new BitmapData(10, 10, false, Color.Red);
+    var box = new BitmapData(200, 60, false, Color.Red);
     boxBitmap = new Bitmap(box);
-    boxBitmap.x = 100 * mIndex;
+    boxBitmap.x = 350 * mIndex;
     boxBitmap.y = 0;
     addChild(boxBitmap);
 
 
-
-
-    var textField1 = new TextField();
+    textField1 = new TextField();
     var textFormat1 = new TextFormat('Helvetica,Arial', 14, Color.Green, bold: true, italic: true);
     textField1.defaultTextFormat = textFormat1;
     textField1.text = mText;
-    textField1.x = 10 + 100 * mIndex;
+    textField1.x = 350 * mIndex;
     textField1.y = 10;
     textField1.width = 920;
     textField1.height = 20;
     addChild(textField1);
-    
-    
-    mTween = new Tween(this, 0.4, TransitionFunction.easeInCircular);
+
+
 
   }
-  
-  void back(){
-    int newx = boxBitmap.x + (-100 * pointer);
-    print("Slide $mIndex back to $newx");
+
+  void back() {
+    int newx = boxBitmap.x + pointer * 350;
+    print("Slide $mIndex back from ${boxBitmap.x} to $newx");
+    mTween = new Tween(this, 0.4, TransitionFunction.linear);
     mTween.animate.x.to(newx);
-    mTween.animate.y.to(0);
-    mTween.animate.alpha.to(1);
+    mTween.onComplete = () {
+      this.x = newx;
+    };
+
+
     juggler.add(mTween);
   }
-  
-  void forward(){
-    int newx = boxBitmap.x + (100 * pointer);
-    print("Slide $mIndex forward to $newx");
+
+  void forward() {
+    int newx = boxBitmap.x + pointer * 350;
+    print("Slide $mIndex forward from ${boxBitmap.x} to $newx");
+    mTween = new Tween(this, 0.4, TransitionFunction.linear);
     mTween.animate.x.to(newx);
-    mTween.animate.y.to(0);
-    mTween.animate.alpha.to(0.5);
+    mTween.onComplete = () {
+      this.x = newx;
+    };
+
     juggler.add(mTween);
   }
 
@@ -68,30 +73,31 @@ void main() {
   var stage = new Stage(canvas);
   var renderLoop = new RenderLoop();
   renderLoop.addStage(stage);
-  juggler = renderLoop.juggler;
+  juggler = stage.juggler;
 
-  Slide slide1 = new Slide("Prima slide", 1);
-  Slide slide2 = new Slide("Seconda slide", 2);
-  Slide slide3 = new Slide("Terza slide", 3);
+  Slide slide1 = new Slide("Prima slide", 0);
+  Slide slide2 = new Slide("Seconda slide", 1);
+  Slide slide3 = new Slide("Terza slide", 2);
 
   stage.addChild(slide1);
   stage.addChild(slide2);
   stage.addChild(slide3);
 
   stage.focus = stage;
- 
+
   stage.onKeyDown.listen((ev) {
 
     switch (ev.keyCode) {
       case 38:
-        pointer += 1;
+        pointer++;
+
         slide1.back();
         slide2.back();
         slide3.back();
 
         break;
       case 40:
-        pointer -= 1;
+        pointer--;
 
         slide1.forward();
         slide2.forward();
